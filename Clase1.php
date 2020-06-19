4de9a26e1674312a106e26b79f39987a00ef4205
71803880pepa <67161849+71803880pepa@users.noreply.github.com>
2020-06-19 11:31:10 -0500

    Create Clase1.php

---
+++ Clase1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+<?php
+    $valor=15;
+    echo var_dump($valor==15);
+    echo "<br>";
+    echo var_dump($valor===12);
+ ?>
+
+    </body>
+</html>
